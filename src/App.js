a9d27ffc44b56886f8e994078131f98c6b3c2158
Jon Kang <jon@primemotive.com.au>
2019-08-02 15:36:41 +1000

    video #9 Context With Hooks

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import React, { useRef } from "react";
+import React, { useRef, createContext } from "react";
 import Toggle from "./Toggle";
 import { useTitleInput } from "./hooks/useTitleInput";
+
+export const UserContext = createContext();
 
 const App = () => {
   const [name, setName] = useTitleInput("");
   const ref = useRef();
 
   return (
-    // attatch the ref to a DOM element
-    <div className="main-wrapper" ref={ref}>
-      <h1 onClick={() => ref.current.classList.add("new-fake-class")}>
-        Level Up Dishes
-      </h1>
-      <Toggle />
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          formSubmit(name, setName);
-        }}
-      >
-        <input
-          type="text"
-          onChange={e => setName(e.target.value)}
-          placeholder="Enter something here"
-          value={name}
-        />
-        <button>Submit</button>
-      </form>
-    </div>
+    <UserContext.Provider
+      value={{
+        user: false
+      }}
+    >
+      {/* attatch the ref to a DOM element */}
+      <div className="main-wrapper" ref={ref}>
+        <h1 onClick={() => ref.current.classList.add("new-fake-class")}>
+          Level Up Dishes
+        </h1>
+        <Toggle />
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            formSubmit(name, setName);
+          }}
+        >
+          <input
+            type="text"
+            onChange={e => setName(e.target.value)}
+            placeholder="Enter something here"
+            value={name}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    </UserContext.Provider>
   );
 };
 
